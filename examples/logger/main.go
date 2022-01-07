14ff5fee4be4a7eabc871951ceb98ffea1b4d4c8
Piotr Persona <persona.piotr@gmail.com>
2022-01-07 21:27:27 +0100

    feat: Add logger example
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

---
+++ examples/logger/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"github.com/piotrpersona/goq/pkg/goq"
+)
+
+type Logger struct {
+	topic goq.Topic
+	q *goq.Queue[time.Time, string]
+}
+
+func NewLogger(q *goq.Queue[time.Time, string]) (logger *Logger) {
+	var topic goq.Topic = "logs"	
+	q.CreateTopic(topic)
+
+	logger = &Logger{
+		topic: topic,
+		q: q,
+	}
+
+	q.Subscribe(topic, "logger", logger)
+
+	return
+}
+
+func (l *Logger) Info(message string) {
+	msg := goq.Message[time.Time, string]{Key: time.Now(), Value: message}
+	l.q.Publish(l.topic, msg)
+}
+
+func (l *Logger) Handle(msg goq.Message[time.Time, string]) (err error) {
+	fmt.Printf("%s %s\n", msg.Key.UTC().Format(time.RFC3339), msg.Value)
+	return
+}
+
+func main() {
+	q := goq.New[time.Time, string]()
+	logger := NewLogger(q)
+
+	logger.Info("hello!")
+	
+	time.Sleep(time.Millisecond*1)
+}
