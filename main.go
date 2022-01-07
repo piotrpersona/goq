5b7a1f0f934bfe465e477b0f36e1cd7328ba2294
Piotr Persona <persona.piotr@gmail.com>
2022-01-07 15:20:56 +0100

    feat: Add basic sub-sub example
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 	"github.com/piotrpersona/goq/pkg/goq"
 )
 
@@ -12,8 +13,18 @@ func main() {
 
 	_ = q.CreateTopic(topic)
 
-	q.Subscribe(topic, "example-subscriber")
+	channel, _ := q.Subscribe(topic, "example-subscriber")
 
 	fmt.Println(q.Topics())
 	fmt.Println(q.Subscribers(topic))
+
+	go func() {
+		select {
+		case msg := <- channel:
+			fmt.Printf("Received message from %s, key: %v value: %v\n", topic, msg.Key, msg.Value)
+		}
+	}()
+	q.Publish(topic, goq.Message[int, string]{1, "hello!"})
+
+	time.Sleep(time.Second *3)
 }
--- pkg/goq/publish.go
+++ pkg/goq/publish.go
@@ -1,5 +1,12 @@
 package goq
 
 func (q *Queue[K, V]) Publish(topic Topic, msg Message[K, V]) (err error) {
+	subscribers := q.topics[topic]
+
+	for _, subGroup := range subscribers {
+		go func() {
+			subGroup.channel <- msg
+		}()
+	}
 	return
 }
